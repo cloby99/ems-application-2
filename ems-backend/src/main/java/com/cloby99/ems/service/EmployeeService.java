0d1f5db758f2b2a9e8127bc1bbbefdb694ead9bd
cloby99 <2020cs020@stu.ucsc.cmb.ac.lk>
2024-03-26 20:55:18 +0530

    Implement Get, Update, Delete Employee REST APIs

--- ems-backend/src/main/java/com/cloby99/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/com/cloby99/ems/service/EmployeeService.java
@@ -2,6 +2,16 @@ package com.cloby99.ems.service;
 
 import com.cloby99.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
 }
--- ems-backend/src/main/java/com/cloby99/ems/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/cloby99/ems/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,15 @@ package com.cloby99.ems.service.impl;
 
 import com.cloby99.ems.dto.EmployeeDto;
 import com.cloby99.ems.entity.Employee;
+import com.cloby99.ems.exception.ResourceNotFoundException;
 import com.cloby99.ems.mapper.EmployeeMapper;
 import com.cloby99.ems.repository.EmployeeRepository;
 import com.cloby99.ems.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -19,5 +23,44 @@ public class EmployeeServiceImpl implements EmployeeService  {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee is not found with given id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(EmployeeMapper::mapToEmployeeDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not found with given id: " + employeeId)
+        );
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not found with given id: " + employeeId)
+        );
+
+        employeeRepository.deleteById(employeeId);
     }
 }
