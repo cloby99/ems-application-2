3e276b49f30679d973ef60af4bdaf0afd9ca4e30
cloby99 <2020cs020@stu.ucsc.cmb.ac.lk>
2024-03-28 14:01:53 +0530

    Created add Employee feature

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ListEmployee from "./components/ListEmployee";
+import AddEmployee from "./components/AddEmployee";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element = {<ListEmployee/>}></Route>
           <Route path="/employees" element = {<ListEmployee/>}></Route>
-
+          <Route path="/add-employee" element = {<AddEmployee/>}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
--- ems-frontend/src/components/ListEmployee.jsx
+++ ems-frontend/src/components/ListEmployee.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { listEmployees } from '../services/EmployeeService'
+import { useNavigate } from 'react-router-dom'
 
 function ListEmployee() {
 
     const [employees, setEmployees] = useState([])
+
+    const navigator = useNavigate();
 
     useEffect(() => {
         listEmployees().then((response) => {
@@ -13,6 +16,10 @@ function ListEmployee() {
         })
 
     }, [])
+
+    function addNewEmployee() {
+        navigator('/add-employee')
+    }
 
     // const dummyData = [
     //     {
@@ -45,6 +52,7 @@ function ListEmployee() {
   return (
     <div className='container'>
         <h2 className='text-center my-4'> List of Employees</h2>
+        <button className='btn btn-warning my-3' onClick={addNewEmployee}>Add Employee</button>
         <table className='table table-dark table-striped table-bordered'>
             <thead>
                 <tr>
